42503819870a4f3106b32c2d17a2ccf17f2a04dc
abithavalli.offl <abithavalli.offl@gmail.com>
2021-02-11 14:18:09 +0530

    BMI web app with flask and firebase

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 #!python3
-
+import os
+import uuid
+from flask import Flask, request, jsonify
+from firebase_admin import credentials, firestore, initialize_app
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+cred = credentials.Certificate('serviceAccountKey.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+users_bmi = db.collection('usersbmi')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     bmi = ''
@@ -11,11 +18,21 @@ def index():
         weight = float(request.form.get('weight'))
         height = float(request.form.get('height'))
         bmi = calc_bmi(weight, height)
+        create(weight,height,bmi)
     return render_template("bmi_calc.html",
 	                        bmi=bmi)
-
+@app.route('/add', methods=['POST'])
+def create(weight1,height1,bmi1):
+    try:
+        json={'bmi': bmi1, 'height': height1, 'weight':weight1}
+        id = uuid.uuid1()
+        users_bmi.document(str(id)).set(json)
+        return json
+    except Exception as e:
+        return f"An Error Occurred: {e}"
 def calc_bmi(weight, height):
     return round((weight / ((height / 100) ** 2)), 2)
 
+port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':
     app.run()
